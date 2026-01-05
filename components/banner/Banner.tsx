6ee6c09316c6f44cf45c5825f068b5ff36017cfa
SrivalliYaarlagadda <srivalli.aiml.rymec@gmail.com>
2026-01-05 11:39:57 +0530

    Changed layout section and banner section

--- components/banner/Banner.tsx
+++ components/banner/Banner.tsx
@@ -1,51 +1,35 @@
 "use client";
-
 import Image from "next/image";
 import { useEffect, useState } from "react";
-
-const desktopImages = [
-  "/bannerImages/banner1.webp",
-  "/bannerImages/banner2.webp",
-  "/bannerImages/banner3.webp",
-];
-
-const mobileImages = [
-  "/bannerImages/BI 1.webp",
-  "/bannerImages/BI 2.webp",
-  "/bannerImages/BI 3.webp",
-];
-
+import { BANNER_IMAGES } from "@/const/bannerImages"; 
 export default function Banner() {
   const [active, setActive] = useState(0);
 
   useEffect(() => {
     const timer = setInterval(() => {
-      setActive((prev) => (prev + 1) % desktopImages.length);
+      setActive((prev) => (prev + 1) % BANNER_IMAGES.length);
     }, 5000);
     return () => clearInterval(timer);
   }, []);
 
   return (
-    <section className="relative -mt-[100px] w-full h-[100vh] overflow-hidden">
-      {desktopImages.map((_, index) => (
+<section className="relative pt-24 w-full h-[100vh] overflow-hidden">
+      {BANNER_IMAGES.map((image, index) => (
         <div
           key={index}
           className={`absolute inset-0 transition-opacity duration-700 ${
             active === index ? "opacity-100 z-10" : "opacity-0 z-0"
           }`}
         >
-          {/* DESKTOP IMAGE (>=768px) */}
           <Image
-            src={desktopImages[index]}
+            src={image.desktop}
             alt="Banner"
             fill
             priority={index === 0}
             className="hidden md:block object-cover object-top"
           />
-
-          {/* MOBILE IMAGE (<768px) */}
           <Image
-            src={mobileImages[index]}
+            src={image.mobile}
             alt="Banner"
             fill
             priority={index === 0}
@@ -53,9 +37,8 @@ export default function Banner() {
           />
         </div>
       ))}
-
       <div className="absolute bottom-6 left-1/2 -translate-x-1/2 z-20 flex gap-3">
-        {desktopImages.map((_, i) => (
+        {BANNER_IMAGES.map((_, i) => (
           <button
             key={i}
             onClick={() => setActive(i)}

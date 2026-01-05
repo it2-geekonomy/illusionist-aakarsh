3fdf7f68c6728183bc96d63be6b16855c333b9d5
gopesh <gopesh@geekonomy.in>
2026-01-05 17:59:18 +0530

    Add PublicShowsSection and RequestAvailabilitySection to the main page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import Banner from "@/components/banner/Banner";
 import VideosSection from "../components/videos-section/VideosSection";
+import PublicShowsSection from "../components/public-shows/PublicShowsSection";
+import RequestAvailabilitySection from "../components/request-availability/RequestAvailabilitySection";
 
 export default function Home() {
   return (
     <main className="min-h-screen">
       <Banner/>
       <VideosSection />
+      <PublicShowsSection />
+      <RequestAvailabilitySection />
     </main>
   );
 }

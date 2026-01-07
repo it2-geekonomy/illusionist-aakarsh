704054eb33503407492cebaf21ffc417bede40b4
Kishan <kishan@geekonomy.in>
2026-01-07 10:17:45 +0530

    about page done

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,15 @@ import Banner from "@/components/banner/Banner";
 import VideosSection from "../components/videos-section/VideosSection";
 import PublicShowsSection from "../components/public-shows/PublicShowsSection";
 import RequestAvailabilitySection from "../components/request-availability/RequestAvailabilitySection";
+import About from "@/components/about";
+import ProfileSection from "@/components/profile-section";
 
 export default function Home() {
   return (
     <main className="min-h-screen">
       <Banner/>
+      <About />
+      <ProfileSection />
       <VideosSection />
       <PublicShowsSection />
       <RequestAvailabilitySection />

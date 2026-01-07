f0eb2028440f68b6943f211234070fb4da3a2e3f
SrivalliY-design <srivalli.aiml.rymec@gmail.com>
2026-01-07 15:52:54 +0530

    Added footer and navigation

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -14,19 +14,22 @@ export default function Footer() {
       window.scrollTo({ top: 0, behavior: "smooth" });
     }
   };
+
   return (
     <footer
-      className="bg-black text-white py-10" style={{ borderTop: "2px solid gray" }}>
-      <div className="w-full max-w-[1200px] mx-auto flex flex-col items-center gap-4">
+      className="bg-black text-white py-10"
+      style={{ borderTop: "2px solid gray", borderBottom: "2px solid gray" }}
+    >
+      <div className="w-full flex flex-col items-center gap-4 px-4">
+
         <div className="flex flex-col items-center p-1 mt-2" style={{ border: "2px solid white" }}>
           <Image
             src="/navlogo.png"
             alt="Illusionist Axe Logo"
-            width={200}
-            height={200}
+            width={300}
+            height={300}
           />
         </div>
-
         <Small className="mt-1 text-center">
           World-Class Corporate Illusion Entertainment
         </Small>
@@ -36,11 +39,13 @@ export default function Footer() {
           className="w-full"
           style={{ height: "1px", backgroundColor: "gray" }}
         />
+
         <div className="w-full px-6 mt-4 flex">
-          <Small className="text-sm text-gray-400 mx-auto sm:mx-0">
+          <Small className="text-sm text-white mx-auto sm:mx-0">
             © {currentYear} Illusionist Axe. All rights reserved.
           </Small>
         </div>
+
         <div className="hidden lg:block h-6"></div>
       </div>
     </footer>
